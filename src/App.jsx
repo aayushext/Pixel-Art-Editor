7d7dc23369540b2f06c0d47971cee1de85a0c805
aayushak178 <aayushak178@gmail.com>
2024-07-15 03:37:31 +0000

    Created a Modal System that contains create new canvas and more

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { useState, useRef, useEffect } from "react";
 // import { Modal } from "./components/Modal";
 import { hex2rgb } from "./components/Utils";
 import "./App.css";
+import Modal from "./components/Modal/Modal";
 
 function App() {
   const canvasRef = useRef(null);
+  const setUpModal = useRef(null);
+
   const [isDragging, setIsDragging] = useState(false);
   const [canvasHeight, setCanvasHeight] = useState(16);
   const [canvasWidth, setCanvasWidth] = useState(16);
+  const [drawColor, setDrawColor] = useState("#ffffff");
   const [canvasColor, setCanvasColor] = useState("#ffffff");
-  const [canvasTransparency, setCanvasTransparency] = useState(null);
+  const [canvasTransparency, setCanvasTransparency] = useState(255);
   const [canvasData, setCanvasData] = useState(null);
-  const [imageData, setImageData] = useState(new Image());
+  var [imageData, setImageData] = useState(new Image());
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -23,9 +27,6 @@ function App() {
     });
     ctx.canvas.width = canvasWidth;
     ctx.canvas.height = canvasHeight;
-    // ctx.fillStyle = "#fff";
-    // ctx.globalAlpha = canvasTransparency / 255;
-    // ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     if (canvasData) {
       handleLoadCanvas();
@@ -36,7 +37,6 @@ function App() {
     }
 
     const handleClick = (event) => {
-      // console.log("Handled Click");
       const rect = canvasRef.current.getBoundingClientRect();
       const scaleFactor = canvasRef.current.width / rect.width;
       const x =
@@ -46,7 +46,7 @@ function App() {
       const ctx = canvasRef.current.getContext("2d");
       const imageData = ctx.getImageData(x, y, 1, 1);
       const data = imageData.data;
-      const color = hex2rgb(canvasColor);
+      const color = hex2rgb(drawColor);
       data[0] = color.r;
       data[1] = color.g;
       data[2] = color.b;
@@ -65,7 +65,7 @@ function App() {
     };
 
     const handleMouseMove = (event) => {
-      setCanvasColor(document.getElementById("color").value);
+      setDrawColor(document.getElementById("color").value);
       if (isDragging) {
         handleClick(event);
       }
@@ -92,6 +92,7 @@ function App() {
     imageData,
     canvasWidth,
     canvasHeight,
+    drawColor,
     canvasColor,
     canvasData,
     isDragging,
@@ -99,15 +100,15 @@ function App() {
   ]);
 
   const handleLoadCanvas = () => {
-    setCanvasHeight(parseInt(document.getElementById("height").value, 10));
-    setCanvasWidth(parseInt(document.getElementById("width").value, 10));
-    setCanvasColor(document.getElementById("color").value);
+    setDrawColor(document.getElementById("color").value);
     setCanvasTransparency(document.getElementById("transparency").value);
   };
 
   const newCanvas = () => {
     setImageData(new Image());
     setCanvasData(null);
+    setCanvasHeight(parseInt(document.getElementById("height").value, 10));
+    setCanvasWidth(parseInt(document.getElementById("width").value, 10));
     handleLoadCanvas();
   };
 
@@ -123,33 +124,13 @@ function App() {
       <div className="container">
         <div className="canvas-setup">
           <div>
-            <p>Height:</p>
-            <input
-              id="height"
-              title="Height"
-              type="number"
-              placeholder="Height"
-              defaultValue={16}
-            ></input>
-          </div>
-          <div>
-            <p>Width:</p>
-            <input
-              id="width"
-              title="Width"
-              type="number"
-              placeholder="Width"
-              defaultValue={16}
-            ></input>
-          </div>
-          <div>
             <p>Color:</p>
             <input
               id="color"
               title="Color"
               type="color"
               placeholder="Color"
-              defaultValue={canvasColor}
+              defaultValue={drawColor}
             ></input>
           </div>
           <div>
@@ -166,15 +147,101 @@ function App() {
           </div>
         </div>
         <div>
-          <button onClick={newCanvas}>New</button>
+          <button
+            onClick={() => {
+              setUpModal.current.open();
+            }}
+          >
+            New
+          </button>
           <button onClick={handleLoadCanvas}>Resize</button>
           <button onClick={saveCanvas}>Save</button>
-          {/* <Modal show={this.state.show} handleClose={this.hideModal}>
-            <p>Modal</p>
+          <Modal ref={setUpModal}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <p>Height:</p>
+                <input
+                  id="height"
+                  title="Height"
+                  type="number"
+                  placeholder="Height"
+                  defaultValue={16}
+                  style={{ marginLeft: "10px" }}
+                ></input>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <p>Width:</p>
+                <input
+                  id="width"
+                  title="Width"
+                  type="number"
+                  placeholder="Width"
+                  defaultValue={16}
+                  style={{ marginLeft: "10px" }}
+                ></input>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <p>Canvas Color: </p>
+                <input
+                  id="color"
+                  title="Color"
+                  type="color"
+                  placeholder="Color"
+                  defaultValue={canvasColor}
+                  onChange={(e) => {
+                    setCanvasColor(e.target.value);
+                  }}
+                  style={{ marginLeft: "10px" }}
+                ></input>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  margin: "20px",
+                }}
+              >
+                <button
+                  onClick={() => {
+                    newCanvas();
+                    setUpModal.current.close();
+                  }}
+                >
+                  Create
+                </button>
+                <button
+                  onClick={() => {
+                    setUpModal.current.close();
+                  }}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
           </Modal>
-          <button type="button" onClick={this.showModal}>
-            Open
-          </button> */}
         </div>
         <div id="editor">
           <canvas id="myCanvas" ref={canvasRef} />
